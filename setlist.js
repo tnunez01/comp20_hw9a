96f90cfd4c67851cecd2f125222f1ef8d2473507
Theo Nunez <60192354+tnunez01@users.noreply.github.com>
2020-11-09 19:54:40 -0600

    Add files via upload

---
+++ setlist.js
@@ -0,0 +1,64 @@
+setlist = {
+    "songs" : [
+        {
+            "title"  : "Breathe",
+            "artist" : "Faith Hill",
+            "genres" : ["Country", "Pop"],
+            "year"   : 2000
+        },
+        {
+            "title"  : "Smooth ft. Rob Thomas",
+            "artist" : "Santana",
+            "genres" : ["Latin","Rock","Alternative"],
+            "year"   : 1999
+        },
+        {
+            "title"  : "Maria Maria ft. The Product G&B",
+            "artist" : "Santana",
+            "genres" : ["Latin", "Pop", "R&B"],
+            "year"   : 1999
+        },
+        {
+            "title"  : "I Wanna Know",
+            "artist" : "Joe",
+            "genres" : ["R&B"],
+            "year"   : 1999
+        },
+        {
+            "title"  : "Everything You Want",
+            "artist" : "Vertical Horizon",
+            "genres" : ["Alternative", "Rock", "Post-grunge"],
+            "year"   : 2000
+        },
+        {
+            "title"  : "Say My Name",
+            "artist" : "Destiny's Child",
+            "genres" : ["R&B", "Pop"],
+            "year"   : 2000
+        },
+        {
+            "title"  : "I Knew I Loved You",
+            "artist" : "Savage Garden",
+            "genres" : ["Pop", "Contemporary"],
+            "year"   : 1999
+        },
+        {
+            "title"  : "Amazed",
+            "artist" : "Lonestar",
+            "genres" : ["Country", "Alternative", "Rock"],
+            "year"   : 2000
+        },
+        {
+            "title"  : "Bent",
+            "artist" : "Matchbox Twenty",
+            "genres" : ["Rock"],
+            "year"   : 2000
+        },
+        {
+            "title"  : "He Wasn't Man Enough",
+            "artist" : "Toni Braxton",
+            "genres" : ["Dance", "Pop"],
+            "year"   : 2000
+        }
+    ]
+}
